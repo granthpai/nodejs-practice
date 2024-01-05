ef20eabdbab2f89253475d39461d6d57d8d25351
Granth Pai <112540499+granthpai@users.noreply.github.com>
2024-01-05 11:42:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+const jwt = require('jsonwebtoken');
+const { findConfigFile } = require('typescript');
+const jwtPassword = 'secret';
+const zod = require("zod");
+
+const emailSchema = zod.string().email();
+const passwordSchema = zod.string().min(6);
+
+function signJwt(username , password){
+    const usernameResponse = emailSchema.safeParse(username);
+    const passwordResponse = passwordSchema.safeParse(password);
+    if (!usernameResponse.success || !passwordResponse.success){
+        return null;
+    }
+    const signature = jwt.sign({username}, jwtPassword)
+    return signature;
+}
+
+function verifyJwt(token){
+    let ans = true;
+    try{
+        jwt.verify(token , password);
+        return true;
+    } catch (e){
+        ans = false
+    }    
+    return ans;
+}
+
+function decodeJwt(token){
+    const decoded = jwt.decode(token);
+    if (decoded){
+        return true;
+    } else {
+        return false;    
+ }
+}
+
+module.exports = {
+  signJwt,
+  verifyJwt,
+  decodeJwt,
+  jwtPassword,
+};
+
+
